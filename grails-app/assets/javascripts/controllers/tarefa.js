d43e259ea0a2e3dc9839c1b44012a8ac0b6a4c91
Giovanni Nicoletti <giovanni.nic@gmail.com>
2017-09-12 01:32:36 -0300

    Trabalho

--- grails-app/assets/javascripts/controllers/tarefa.js
+++ grails-app/assets/javascripts/controllers/tarefa.js
@@ -3,48 +3,100 @@ var tarefa = new Vue({
     data: {
         tarefas: [],
         tarefa: {},
-        loading: false
+
+        logsTarefa: [],
+        logTarefa: {},
+
+        loading: false,
+        somenteLeitura: false
     },
     methods: {
-        getTarefas: function(){
+        getTarefas: function() {
             this.loading = true;
-            this.$http.get(window.baseUrl+"tarefa/list/").then(function(resp){
+            this.$http.get(window.baseUrl + "tarefa/list/").then(function(resp) {
                 this.tarefas = resp.data;
                 this.loading = false;
-            }, function(err){
+            }, function(err) {
                 this.loading = false;
             })
         },
-        novaTarefa: function(){
+        buscaTarefas: function() {
+            console.log('buscaTarefas...');
+            this.loading = true;
+            this.tarefas = []
+            this.$http
+                .get(window.baseUrl + "tarefa/buscaGeralTarefa/", this.tarefa)
+                .then(
+                    function(resp) {
+                        this.tarefas = resp.data;
+                        this.loading = false;
+                    },
+                    function(err) {
+                        this.loading = false;
+                    })
+        },
+        novaTarefa: function() {
             this.tarefa = {};
+            this.tarefa.dataLimite = moment().add(1, 'days').format("DD/MM/YYYY");
+            this.tarefa.porcentagem = 0;
+            this.somenteLeitura = false;
             $("#formTarefa").modal('show');
         },
-        salvarTarefa: function(){
-            this.$http.post(window.baseUrl+"tarefa/save", this.tarefa).then(function(resp){
+        salvarTarefa: function() {
+            this.$http.post(window.baseUrl + "tarefa/save", this.tarefa).then(function(resp) {
                 this.getTarefas();
                 $("#formTarefa").modal('hide');
                 this.tarefa = {};
-            }, function(error){
+            }, function(error) {
                 console.info(error)
             })
         },
-        editTarefa: function(tarefa){
-            this.$http.get(window.baseUrl+"tarefa/show/"+tarefa.id).then(function(resp){
+        editTarefa: function(tarefa) {
+            this.$http.get(window.baseUrl + "tarefa/show/" + tarefa.id).then(function(resp) {
                 this.tarefa = resp.data;
+                this.somenteLeitura = false;
                 $("#formTarefa").modal('show');
-            }, function(resp){
-            })
+            }, function(resp) {})
         },
-        updateTarefa: function(){
-            this.$http.put(window.baseUrl+"tarefa/update/"+this.tarefa.id, this.tarefa).then(function(resp){
+        updateTarefa: function() {
+            this.$http.put(window.baseUrl + "tarefa/update/" + this.tarefa.id, this.tarefa).then(function(resp) {
                 this.getTarefas();
+                this.somenteLeitura = false;
                 $("#formTarefa").modal('hide');
                 this.tarefa = {};
-            }, function(resp){
-            })
+            }, function(resp) {})
+        },
+        visualizarTarefa: function(tarefa) {
+            this.$http.get(window.baseUrl + "tarefa/show/" + tarefa.id).then(function(resp) {
+                    this.tarefa = resp.data;
+                },
+                this.$http.get(window.baseUrl + "logTarefa/list/" + tarefa.id).then(function(resp) {
+                    this.logsTarefa = resp.data;
+                    this.somenteLeitura = true;
+                    $("#formVisualizar").modal('show');
+                }),
+                function(resp) {
+                    console.info(error)
+                })
+        },
+        novoLogTarefa: function() {
+            this.logTarefa = {};
+            this.logTarefa.tarefa = this.tarefa;
+            this.somenteLeitura = false;
+            $("#formVisualizar").modal('hide');
+            $("#formLogTarefa").modal('show');
+        },
+        salvarLog: function() {
+            this.$http.post(window.baseUrl + "logTarefa/save", this.logTarefa).then(function(resp) {
+                    $("#formLogTarefa").modal('hide');
+                    this.visualizarTarefa(this.tarefa);
+                },
+                function(error) {
+                    console.info(error)
+                })
         }
     },
-    ready: function(){
+    ready: function() {
         this.getTarefas();
     }
 });
--- grails-app/controllers/br/edu/unirn/TarefaController.groovy
+++ grails-app/controllers/br/edu/unirn/TarefaController.groovy
@@ -8,11 +8,16 @@ class TarefaController {
 
     def index(){}
 
+    def buscaTarefa(){
+
+    }
+
     def save(){
         params << request.JSON
         Tarefa tarefa = new Tarefa()
         bindData(tarefa, params, [exclude:['dataLimite']])
         tarefa.dataLimite = params.date('dataLimite', 'dd/MM/yyyy')
+        tarefa.setUsuarioAbertura(session.usuario)
 
         if(!tarefa.save(flush: true)){
             tarefa.errors.each {println it}
@@ -30,9 +35,12 @@ class TarefaController {
                 titulo: tarefa.titulo,
                 texto: tarefa.texto,
                 usuarioAbertura: tarefa.usuarioAbertura.id,
-                usuarioResponsavel: tarefa.usuarioResponsavel?.id,
+                emailUsuarioAbertura: tarefa.usuarioAbertura.email,
+                usuarioResponsavel: tarefa.usuarioResponsavel.id,
+                emailUsuarioResponsavel: tarefa.usuarioResponsavel.email,
                 dataLimite: tarefa.dataLimite.format("dd/MM/yyyy"),
                 tipoTarefa: tarefa.tipoTarefa.id,
+                descricaoTipoTarefa: tarefa.tipoTarefa.descricao,
                 statusTarefa: tarefa.statusTarefa.name(),
                 porcentagem: tarefa.porcentagem
         ] as JSON)
@@ -41,7 +49,7 @@ class TarefaController {
     def list(){
         def retorno = []
 
-        Tarefa.list().each {
+        Tarefa.listOrderById().each {
             retorno.add([
                 id: it.id,
                 titulo: it.titulo,
@@ -53,7 +61,7 @@ class TarefaController {
                 porcentagem: it.porcentagem
             ])
         }
-
+0
         render retorno as JSON
     }
 
@@ -70,5 +78,70 @@ class TarefaController {
         }
 
         render status: 200
+    }
+
+    def buscaPorUsuarioCadastro(){
+        Tarefa tarefa = Tarefa.findByUsuarioCadastro(params.usuarioCadastro);
+    }
+
+    def buscaGeralTarefa(){
+
+        def retorno = []
+        def temp = []
+
+        params << request.JSON
+
+        def titulo = "%" + params.titulo + "%"
+        def texto = params.texto
+        def usuarioAbertura = params.usuarioAbertura
+        def usuarioResponsavel = params.usuarioResponsavel
+        def tipo = params.tipoTarefa
+        def status = params.statusTarefa
+        def criteria = Tarefa.createCriteria()
+
+        temp = criteria.list {
+            if (titulo){
+                ilike("titulo", titulo)
+            }
+            if (texto) {
+                ilike("texto","%$texto%")
+            }
+            if (usuarioAbertura) {
+                usuarioAbertura{
+                    ilike("email","%$usuarioAbertura%")
+                }
+            }
+            if (usuarioResponsavel) {
+                usuarioResponsavel {
+                    ilike("email","%$usuarioResponsavel%")
+                }
+            }
+            if (tipo) {
+                tipoTarefa {
+                    ilike("descricao", "%$tipo%")
+                }
+            }
+            if (status) {
+                tipoTarefa {
+                    ilike("descricao", "%$status%")
+                }
+            }
+        }
+
+        temp.each {
+            retorno.add([
+                id: it.id,
+                titulo: it.titulo,
+                usuarioAbertura: it.usuarioAbertura.email,
+                usuarioResponsavel: it.usuarioResponsavel?.email,
+                dataLimite: it.dataLimite.format("dd/MM/yyyy"),
+                tipoTarefa: it.tipoTarefa.descricao,
+                statusTarefa: it.statusTarefa.name(),
+                porcentagem: it.porcentagem
+            ])
+        }
+
+        render retorno as JSON
+
     }
 }
